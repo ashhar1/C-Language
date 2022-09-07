c8f9f9dab70dd88d0797b4a25f3de079c7b8210a
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-07 13:43:14 +0530

    Create 2_Addition_operation.c

---
+++ 2_Addition_operation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int a, b, c;
+  printf("Enter the 2 number:\n");
+  scanf("%d %d", &a, &b);
+  c = a + b;
+  printf("The addition of 2 number is: %d", c);
+  return 0;
+}
