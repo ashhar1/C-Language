e8345105c039e2018371e77e5b5aece7a53b6645
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-10 17:18:43 +0530

    Create 24_Multiplication_table.c

---
+++ 24_Multiplication_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of which you want the multiplication table: \n");
+    scanf("%d", &n);
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
